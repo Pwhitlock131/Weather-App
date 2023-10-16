e152aea9a90fd8a62494b21a8886226ff435ea78
Pwhitlock <pwhitlock131@gmail.com>
2023-10-15 22:05:51 -0500

    added bootstrap to HTML, and API Key to javascript

---
+++ Develop/assets/js/script.js
@@ -0,0 +1,7 @@
+var container
+
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={83daaadc82f3f3d7bdcb9ebd24147bf8}
+// a78ba7a969b051c867faecaa891b95d1 Default Key???
+
+
+// 83daaadc82f3f3d7bdcb9ebd24147bf8 generated key
