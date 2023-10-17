ce98a46b97565c6ba8f55aeb588143be9b4620b3
Pwhitlock <pwhitlock131@gmail.com>
2023-10-17 18:14:41 -0500

    Added variables for apikey, cities, and the queryURL

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,7 +1,14 @@
-var container
+var APIKey = "83daaadc82f3f3d7bdcb9ebd24147bf8";
+var city = "Dallas";
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+fetch(queryURL)
+console.log(queryURL)
+
 
 // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={83daaadc82f3f3d7bdcb9ebd24147bf8}
 // a78ba7a969b051c867faecaa891b95d1 Default Key???
 
 
 // 83daaadc82f3f3d7bdcb9ebd24147bf8 generated key
+
