c86d40591cdd8c3e3f7f62a7d018103222237b19
Pwhitlock <pwhitlock131@gmail.com>
2023-10-18 19:16:55 -0500

    Added functions to js

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,9 +1,27 @@
 var APIKey = "83daaadc82f3f3d7bdcb9ebd24147bf8";
 var city = "Dallas";
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var searchedCity = document.querySelector("#search-bar").value
+var searchForm = document.querySelector("#search-form")
+
+
+
+
 
 fetch(queryURL)
 console.log(queryURL)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={83daaadc82f3f3d7bdcb9ebd24147bf8}
@@ -12,3 +30,7 @@ console.log(queryURL)
 
 // 83daaadc82f3f3d7bdcb9ebd24147bf8 generated key
 
+searchForm.addEventListener("submit", function(e){
+    e.preventDefault()
+    console.log(searchedCity)
+});
